203db7279cc153760eba9c9edae435bfb0e9746e
vijaytoshniwal <srishyamkolkata@gmail.com>
2026-07-16 15:03:37 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>e.waitUntil(caches.open('nutrition-pulse-v1').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
